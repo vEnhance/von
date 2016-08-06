f97394c848d05f6836a5eb6fdd62f3137159e77d
Evan Chen <evanchen@mit.edu>
2016-08-06 12:06:01 -0500

    Improve tag help text

--- rc.py
+++ rc.py
@@ -1,6 +1,6 @@
 import os
 
-VON_BASE_PATH = "/home/evan/Dropbox/Documents/Oly-Math/Database/" # TODO for public, use better
+VON_BASE_PATH = "/home/evan/Dropbox/OlyBase/" # TODO for public, use better
 VON_INDEX_NAME = "index"
 VON_INDEX_PATH = os.path.join(VON_BASE_PATH, VON_INDEX_NAME)
 VON_CACHE_NAME = "cache"
@@ -13,22 +13,28 @@ NSEPERATOR = '\n' + SEPERATOR + '\n'
 
 TAG_HINT_TEXT = """# Some hints for tags:
 #
-# ** Difficulty: @trivial @aleph @bet @gimel @dalet @zayin @yod @kurumi
-# ** Sources: @mine @obscure @rare @secret @exam @blind
-# ** Problem Shape: @yesno @find @construct @bestpossible @hardanswer
+# *** Difficulty: @trivial @aleph @bet @gimel @dalet @zayin @yod @kurumi
+# *** Sources: @mine @obscure @rare @secret @exam @blind
+# *** Problem Shape: @yesno @find @construct @bestpossible @hardanswer
 #
 # Quality: @favorite > @nice > @good > @ugly @work
 # Philosophy: @instructive @reliable @justdoit @magic
 # Philosophy': @smallcases @equalitycase @scouting @meta @dumb
-# Philosophy'': @wishful @criticalclaim  @stronger @thinkbig
+# Philosophy'': @wishful @criticalclaim @stronger @thinkbig
 # Solution Method: @induct @manysolutions @magic @inefficient  @explicit @compute
 # More tags: @pitfall @troll @intuitive @size @weak @maturity
-# NT tags: @primes @p#adic @QR @pell @smallestprime @mods @fermat @zsig @cyclotomic @divis @order @christmas @CRT
-# Algebra tags: @polynomial @trig @roots @calculus @continuity @irreducible @exactsum @manip
-# Ineq tags: @holder @CDN @schur @AMGM @Titu @homogenize @dehomogenize @SOS @jensen @isofudge
-# Combinatorics: @greedy @optimization @additivecombo @extreme @invariant @pigeonhole @parity @graph @adhoc @EV @combogeo @hall @grid @rigid
+#
+# NT tags: @primes @-#adic @QR @pell @smallestprime @mods
+#          @fermat @zsig @cyclotomic @divis @order @christmas @CRT
+# Algebra tags: @polynomial @trig @roots @calculus
+#          @continuity @irreducible @exactsum @manip
+# Ineq tags: @holder @CDN @schur @AMGM @Titu @homogenize
+#            @dehomogenize @SOS @jensen @isofudge
+# Combinatorics: @greedy @optimization @additivecombo @extreme @invariant
+#                @pigeonhole @parity @graph @adhoc @EV @combogeo @hall @grid @rigid
 # Geometry tags
-  # Part I and II: @anglechase @simtri @pop @homothety @cevalaus @trig @complex @bary @length
+  # Part I and II: @anglechase @simtri @pop @homothety
+  #                @cevalaus @trig @complex @bary @length
   # Part III: @inversion @polar @projective @harmonic @miquel @spiralsim @mixtilinear"""
 
 USE_COLOR = True
