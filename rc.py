708ecd4441749be01998673da3c242509692c776
Evan Chen <evanchen@mit.edu>
2017-01-20 16:14:37 -0500

    New tags in VON

--- rc.py
+++ rc.py
@@ -19,7 +19,7 @@ TAG_HINT_TEXT = """# Some hints for tags:
 # (here @well means I spent a lot of time on the problem myself)
 #
 # Quality: @favorite > @nice > @good > @ugly @work
-# Philosophy: @instructive @reliable @justdoit @magic
+# Philosophy: @instructive @reliable @justdoit @magic @contrived
 # Philosophy': @smallcases @equalitycase @scouting @meta @dumb
 # Philosophy'': @wishful @criticalclaim @stronger @thinkbig
 # Solution Method: @induct @manysolutions @magic @inefficient @explicit @compute
@@ -31,7 +31,7 @@ TAG_HINT_TEXT = """# Some hints for tags:
 #        @continuity @irreducible @exactsum @manip
 # FE:    @cauchy @pointwise @cancel @symmetry @bump @isolated
 # Ineq:  @holder @CDN @schur @AMGM @Titu @homogenize
-#        @dehomogenize @SOS @jensen @isofudge
+#        @dehomogenize @SOS @jensen @isofudge @nEV
 # Combo: @greedy @optimization @additivecombo @extreme @invariant
 #        @free @pigeonhole @parity @graph @adhoc @EV @global @local
 #        @hall @grid @rigid @perturb @algorithm @linalg @combogeo
