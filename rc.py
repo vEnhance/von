c9139ba64076b658d4f4bbdafb950e0cafc70a00
Evan Chen <evanchen@mit.edu>
2017-01-23 11:03:51 -0500

    Remove @ from tags so autocomp works

--- rc.py
+++ rc.py
@@ -13,36 +13,36 @@ NSEPERATOR = '\n' + SEPERATOR + '\n'
 
 TAG_HINT_TEXT = """# Some hints for tags:
 #
-# *** Difficulty: @trivial @aleph @bet @dalet @gimel @zayin @yod @kurumi
-# *** Sources: @mine @obscure @rare @secret @exam @blind @well
-# *** Problem Shape: @yesno @find @construct @bestpossible @hardanswer @aime
-# (here @well means I spent a lot of time on the problem myself)
+# *** Difficulty: trivial aleph bet dalet gimel zayin yod kurumi
+# *** Sources: mine obscure rare secret exam blind well
+# *** Problem Shape: yesno find construct bestpossible hardanswer aime
+# (here well means I spent a lot of time on the problem myself)
 #
-# Quality: @favorite > @nice > @good > @ugly @work
-# Philosophy: @instructive @reliable @justdoit @magic @contrived
-# Philosophy': @smallcases @equalitycase @scouting @meta @dumb
-# Philosophy'': @wishful @criticalclaim @stronger @thinkbig
-# Solution Method: @induct @manysolutions @magic @inefficient @explicit @compute
-# More tags: @pitfall @troll @intuitive @size @weak @maturity
+# Quality: favorite > nice > good > ugly work
+# Philosophy: instructive reliable justdoit magic contrived
+# Philosophy': smallcases equalitycase scouting meta dumb
+# Philosophy'': wishful criticalclaim stronger thinkbig
+# Solution Method: induct manysolutions magic inefficient explicit compute
+# More tags: pitfall troll intuitive size weak maturity
 #
-# NT:    @primes @p-adic @QR @pell @smallestprime @mods @euclid
-#        @fermat @zsig @cyclotomic @divis @order @christmas @CRT
-# Alg:   @polynomial @trig @roots @calculus @genfunc
-#        @continuity @irreducible @exactsum @manip
-# FE:    @cauchy @pointwise @cancel @symmetry @bump @isolated
-# Ineq:  @holder @CDN @schur @AMGM @Titu @homogenize
-#        @dehomogenize @SOS @jensen @isofudge @nEV
-# Combo: @greedy @optimization @additivecombo @extreme @invariant
-#        @free @pigeonhole @parity @graph @adhoc @EV @global @local
-#        @hall @grid @rigid @perturb @algorithm @linalg @combogeo
+# NT:    primes p-adic QR pell smallestprime mods euclid
+#        fermat zsig cyclotomic divis order christmas CRT
+# Alg:   polynomial trig roots calculus genfunc
+#        continuity irreducible exactsum manip
+# FE:    cauchy pointwise cancel symmetry bump isolated
+# Ineq:  holder CDN schur AMGM Titu homogenize
+#        dehomogenize SOS jensen isofudge nEV
+# Combo: greedy optimization additivecombo extreme invariant
+#        free pigeonhole parity graph adhoc EV global local
+#        hall grid rigid perturb algorithm linalg combogeo
 # Geometry tags
-  # Part I and II: @anglechase @simtri @pop @homothety @config
-  #                @cevalaus @trig @complex @bary @length
-  # Part III:      @inversion @polar @projective @harmonic
-  #                @miquel @spiralsim @mixtilinear"""
+  # Part I and II: anglechase simtri pop homothety config
+  #                cevalaus trig complex bary length
+  # Part III:      inversion polar projective harmonic
+  #                miquel spiralsim mixtilinear"""
 
 USE_COLOR = True
 KEY_CHAR = 'C~'
 
-DIFFS= ['trivial', 'aleph', 'bet', 'dalet', 'gimel', 
+DIFFS= ['trivial', 'aleph', 'bet', 'dalet', 'gimel',
 		'zayin', 'yod', 'kurumi']
