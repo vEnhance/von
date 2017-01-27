7c251b17307a72384d06ed743d34e054bff29376
Evan Chen <evanchen@mit.edu>
2017-01-27 18:35:00 -0500

    Better von support

--- strparse.py
+++ strparse.py
@@ -4,25 +4,25 @@ import re
 def demacro(text):
 	# TODO this doesn't quite work, e.g. \epsilon -> \epsilonilon
 	replacements = [
-	  (r"\ii",       r"\item") ,
-	  (r"\wh",       r"\widehat") ,
-	  (r"\ol",       r"\overline"),
-	  (r"\eps",      r"\varepsilon"),
-	  (r"\dang",     r"\measuredangle"),
-	  (r"\dg",       r"^{\circ}"),
-	  (r"\inv",      r"^{-1}"),
-	  (r"\half",     r"\frac{1}{2}"),
-	  (r"\NN",       r"{\mathbb N}"),
-	  (r"\ZZ",       r"{\mathbb Z}"),
-	  (r"\CC",       r"{\mathbb C}"),
-	  (r"\RR",       r"{\mathbb R}"),
-	  (r"\QQ",       r"{\mathbb Q}"),
-	  (r"\FF",       r"{\mathbb F}"),
-	  (r"\opname",   r"\operatorname"),
-	  (r"\defeq",    r"\overset{\text{def}}{=}"),
-	  (r"\id",       r"\operatorname{id}"),
-	  (r"\injto",    r"\hookrightarrow"),
-	  (r"\vdotswithin=", r"\vdots"),
+		(r"\ii",			 r"\item") ,
+		(r"\wh",			 r"\widehat") ,
+		(r"\ol",			 r"\overline"),
+		(r"\eps",			r"\varepsilon"),
+		(r"\dang",		 r"\measuredangle"),
+		(r"\dg",			 r"^{\circ}"),
+		(r"\inv",			r"^{-1}"),
+		(r"\half",		 r"\frac{1}{2}"),
+		(r"\NN",			 r"{\mathbb N}"),
+		(r"\ZZ",			 r"{\mathbb Z}"),
+		(r"\CC",			 r"{\mathbb C}"),
+		(r"\RR",			 r"{\mathbb R}"),
+		(r"\QQ",			 r"{\mathbb Q}"),
+		(r"\FF",			 r"{\mathbb F}"),
+		(r"\opname",	 r"\operatorname"),
+		(r"\defeq",		r"\overset{\text{def}}{=}"),
+		(r"\id",			 r"\operatorname{id}"),
+		(r"\injto",		r"\hookrightarrow"),
+		(r"\vdotswithin=", r"\vdots"),
 	]
 	s = text
 	for short, full in replacements:
@@ -41,6 +41,13 @@ def toAOPS(text):
 	text = text.replace(r"\end{enumerate}", "[/list]")
 	text = text.replace(r"\begin{itemize}", "[list]")
 	text = text.replace(r"\end{itemize}", "[/list]")
+	for env in ['theorem', 'claim', 'lemma', 'proposition', 'corollary']:
+		text = text.replace(r"\begin{" + env + "*}", "\n[b]" + env.title() + "[/b]: ")
+		text = text.replace(r"\end{" + env + "*}", "")
+		text = text.replace(r"\begin{" + env + "}", "\n[b]" + env.title() + "[/b]: ")
+		text = text.replace(r"\end{" + env + "}", "")
+	text = text.replace(r"\begin{proof}", "\n[i]Proof.[/i] ")
+	text = text.replace(r"\end{proof}", r"$\blacksquare$" + "\n")
 	text = text.replace(r"\#", "#")
 	text = re.sub(r"\\emph{([^}]*)}", r"[i]\1[/i]", text)
 	text = re.sub(r"\\textit{([^}]*)}", r"[i]\1[/i]", text)
