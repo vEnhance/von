0075a71bbad7ab21f81c953d3857bc1367138d4e
Evan Chen <evanchen@mit.edu>
2017-05-20 18:19:05 -0400

    Fix an index in API
    Since only support read-only operations

--- api.py
+++ api.py
@@ -5,6 +5,21 @@ import model
 import sys
 sys.modules['model'] = model
 
+index = model.VonIndex().store # get the underlying dict
+
+def has(source):
+	return source in index
+
 def get(source):
-	entry = model.getEntryBySource(source)
-	return None if entry is None else entry.full
+	entry = index[source]
+	return entry.full
+
+def getStatement(source):
+	entry = index[source]
+	return entry.full.bodies[0]
+
+def getSolution(source):
+	entry = index[source]
+	bodies = entry.full.bodies
+	assert len(bodies) > 1, "%s has no solution" % source
+	return bodies[1]
