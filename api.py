583867eac0730b1fa7b4f854f0145243969e811a
Evan Chen <evanchen@mit.edu>
2017-07-05 17:43:11 -0500

    Doc-file for API module

--- api.py
+++ api.py
@@ -8,18 +8,26 @@ sys.modules['model'] = model
 index = model.VonIndex().store # get the underlying dict
 
 def has(source):
+	"""Checks whether a given source exists in database"""
 	return source in index
 
+def has_solution(source):
+	"""Checks whether a given source exists in database AND has a solution"""
+	if not has(source): return False
+	entry = index[source]
+	return len(entry.full.bodies) > 1
+
 def get(source):
+	"""Returns the full data fora given source"""
 	entry = index[source]
 	return entry.full
 
-def getStatement(source):
-	entry = index[source]
-	return entry.full.bodies[0]
+def get_statement(source):
+	"""Returns just the problem statement for a given source"""
+	return get(source).bodies[0]
 
-def getSolution(source):
-	entry = index[source]
-	bodies = entry.full.bodies
+def get_solution(source):
+	"""Returns just the solution for a given source (asserts existence)"""
+	bodies = get(source).bodies
 	assert len(bodies) > 1, "%s has no solution" % source
 	return bodies[1]
