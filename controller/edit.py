448403c5e8fad53dfde69dacbf4600a5e99d5ab8
Evan Chen <evanchen@mit.edu>
2017-07-07 14:17:01 -0500

    Create option to edit all

--- controller/edit.py
+++ controller/edit.py
@@ -5,15 +5,23 @@ import preview
 import subprocess
 
 parser = view.Parser(prog='edit', description='Opens problem(s) by source name.')
-parser.add_argument('keys', nargs='+', help="The key of the problem to open (either source or cache index).")
+parser.add_argument('-a', '--all', action='store_const',
+		dest = 'all', default=False, const=True,
+		help="Edit all problems in cache")
+parser.add_argument('keys', nargs='*',
+		help="The key of the problem to open (either source or cache index).")
 
 def main(self, argv):
 	opts = parser.process(argv)
 	opts.verbose = True
-	for key in opts.keys:
-		entry = model.getEntryByKey(key)
+
+	if opts.all is True:
+		entries = model.readCache()
+	else:
+		entries = [model.getEntryByKey(key) for key in opts.keys]
+	for entry in entries:
 		if entry is None:
-			view.error(key + " not found")
+			view.error("Not found")
 		else:
 			full_path = model.completePath(entry.path)
 			preview.make_preview(full_path)
