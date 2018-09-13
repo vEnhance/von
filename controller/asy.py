c3762eb1dd6a455cedadc50cac590205454425de
Evan Chen <evanchen@mit.edu>
2018-09-13 11:05:36 -0400

    Can grab asy diagrams from diff bodies now
    von asy -b 2 SOURCE will grab diagrams from walkthroughs

--- controller/asy.py
+++ controller/asy.py
@@ -7,6 +7,8 @@ parser.add_argument('key',
 		help = "The key of the problem to open")
 parser.add_argument('-n', '--number', default = 1, type = int,
 		help = "Takes the n-th diagram, one-indexed. Defaults to 1.")
+parser.add_argument('-b', '--body', default = 1, type = int,
+		help = "Takes the diagram from the b'th body. Defaults to 1.")
 parser.add_argument('-c', '--comments', action='store_const',
 		const = True, default = False,
 		help = "Decides whether or not commented lines should be displayed")
@@ -18,7 +20,7 @@ def main(self, argv):
 		view.error(opts.key + " not found")
 	else:
 		problem = entry.full
-		soln = problem.bodies[1]
+		soln = problem.bodies[opts.body]
 		asys = re.findall(r"\\begin\{asy\}(.+?)\\end\{asy\}", soln, flags=re.DOTALL)
 		diagram = asys[opts.number-1]
 
