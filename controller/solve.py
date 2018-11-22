ef7a95454a9f59e06e6d75c5127822a1ee938593
Evan Chen <evanchen@mit.edu>
2018-11-21 19:29:43 -0500

    Option to have page breaks in solves

--- controller/solve.py
+++ controller/solve.py
@@ -5,6 +5,9 @@ parser = view.Parser(prog='solve',\
 		description=r'Taking as input a TeX file, expands \von instances.')
 
 parser.add_argument('filename', help="The filename to translate.")
+parser.add_argument('-p', '--pagebreaks', action='store_const',
+		const = True, default = False,
+		help="Include page break after every solution (default is a bar).")
 parser.add_argument('-l', '--lazy', action='store_const',
 		const = True, default = False,
 		help="Don't include solutions to the problems.")
@@ -42,7 +45,10 @@ def main(self, argv):
 					if len(problem.bodies) > 1:
 						s += r'\subsubsection*{\ul{Solution}}' + '\n'
 						s += model.demacro(problem.bodies[1]) + '\n'
-						s += r'\hrulebar' + '\n'
+						if opts.pagebreaks:
+							s += r'\newpage' + '\n'
+						else:
+							s += r'\hrulebar' + '\n'
 					else:
 						view.warn("No solution to " + key)
 		view.out(s)
