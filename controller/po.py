3db91846c9d026be244a2aa9650709f838fd3238
Evan Chen <evan@evanchen.cc>
2019-08-07 12:37:07 -0700

    Use geometry for preamble, not olympiad/cse5

--- controller/po.py
+++ controller/po.py
@@ -143,18 +143,49 @@ LATEX_PREAMBLE = r"""
 \usepackage{tikz-cd} % diagrams
 \usepackage{asymptote} % more diagrams
 \begin{asydef}
-import olympiad;
-import cse5;
-pointpen = black;
-pathpen = black;
-pathfontpen = black;
-anglepen = black;
-anglefontpen = black;
-pointfontsize = 10;
 defaultpen(fontsize(10pt));
 size(8cm); // set a reasonable default
 usepackage("amsmath");
 usepackage("amssymb");
+settings.tex="latex";
+settings.outformat="pdf";
+// Replacement for olympiad+cse5 which is not standard
+import geometry;
+// recalibrate fill and filldraw for conics
+void filldraw(picture pic = currentpicture, conic g, pen fillpen=defaultpen, pen drawpen=defaultpen)
+	{ filldraw(pic, (path) g, fillpen, drawpen); }
+void fill(picture pic = currentpicture, conic g, pen p=defaultpen)
+	{ filldraw(pic, (path) g, p); }
+// some geometry
+pair foot(pair P, pair A, pair B) { return foot(triangle(A,B,P).VC); }
+pair orthocenter(pair A, pair B, pair C) { return orthocentercenter(A,B,C); }
+pair centroid(pair A, pair B, pair C) { return (A+B+C)/3; }
+// cse5 abbrevations
+path CP(pair P, pair A) { return circle(P, abs(A-P)); }
+path CR(pair P, real r) { return circle(P, r); }
+pair IP(path p, path q) { return intersectionpoints(p,q)[0]; }
+pair OP(path p, path q) { return intersectionpoints(p,q)[1]; }
+path Line(pair A, pair B, real a=0.6, real b=a) { return (a*(A-B)+A)--(b*(B-A)+B); }
+// cse5 more useful functions
+picture CC() {
+	picture p=rotate(0)*currentpicture;
+	currentpicture.erase();
+	return p;
+}
+pair MP(Label s, pair A, pair B = plain.S, pen p = defaultpen) {
+	Label L = s;
+	L.s = "$"+s.s+"$";
+	label(s, A, B, p);
+	return A;
+}
+pair Drawing(Label s = "", pair A, pair B = plain.S, pen p = defaultpen) {
+	dot(MP(s, A, B, p), p);
+	return A;
+}
+path Drawing(path g, pen p = defaultpen, arrowbar ar = None) {
+	draw(g, p, ar);
+	return g;
+}
 \end{asydef}
 
 % The below really should be scrlayer-scrpage
