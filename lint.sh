290f277b804522cb6412212526a648bbd7a743dc
Evan Chen <evan@evanchen.cc>
2023-08-31 21:16:28 -0400

    ci: Improve lint.sh script

--- lint.sh
+++ lint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 readarray -t SPELL_FILES_ARRAY < <(git ls-files)
 codespell "${SPELL_FILES_ARRAY[@]}"
 
-pyright .
-pyflakes .
-
 readarray -t PY_FILES_ARRAY < <(git ls-files '*.py')
+pyright "${PY_FILES_ARRAY[@]}"
+pyflakes "${PY_FILES_ARRAY[@]}"
 black --check "${PY_FILES_ARRAY[@]}"
+isort --check --profile black "${PY_FILES_ARRAY[@]}"
