68980789ab0a1dc6d2836858a288011eae9830be
Evan Chen <evan@evanchen.cc>
2024-01-20 11:41:31 -0500

    ci: Use ruff instaed of pyflakes+black+isort

--- lint.sh
+++ lint.sh
@@ -6,7 +6,6 @@ readarray -t SPELL_FILES_ARRAY < <(git ls-files)
 codespell "${SPELL_FILES_ARRAY[@]}"
 
 readarray -t PY_FILES_ARRAY < <(git ls-files '*.py')
+ruff check "${PY_FILES_ARRAY[@]}"
+ruff format --diff "${PY_FILES_ARRAY[@]}"
 pyright "${PY_FILES_ARRAY[@]}"
-pyflakes "${PY_FILES_ARRAY[@]}"
-black --check "${PY_FILES_ARRAY[@]}"
-isort --check --profile black "${PY_FILES_ARRAY[@]}"
