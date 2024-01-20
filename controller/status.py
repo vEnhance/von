de16e89fd3698122636a0e98e50c2ffa6e3049d1
Evan Chen <evan@evanchen.cc>
2024-01-20 09:07:23 -0500

    fix(status): actually read parser for ss command

--- controller/status.py
+++ controller/status.py
@@ -4,5 +4,6 @@ parser = view.Parser(prog="ss", description="Prints the Cache.")
 
 
 def main(self: object, argv: list[str]):
+    parser.process(argv)
     for entry in model.readCache():
         view.printEntry(entry)
