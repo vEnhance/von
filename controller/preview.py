a577366d9c9926c857b504fccc831e240d6145e0
extouchtriangle <eastonwei12@gmail.com>
2024-10-28 02:13:04 +0000

    Add author (#30)
    * add Mathematical Reflections to puid script
    
    * alphasort
    
    * add author to preview.py
    
    * Update preview.py
    
    * Update preview.py
    
    * Formatted preview.py
    
    ---------
    
    Co-authored-by: Evan Chen <evan@evanchen.cc>

--- controller/preview.py
+++ controller/preview.py
@@ -1,6 +1,6 @@
 import os
 
-from ..rc import USER_OS, VON_PREVIEW_PATH
+from ..rc import USER_OS, VON_DEFAULT_AUTHOR, VON_PREVIEW_PATH
 
 
 def make_preview(full_path: str):
@@ -12,6 +12,7 @@ def make_preview(full_path: str):
     with open(VON_PREVIEW_PATH, "w") as f:
         print(r"\documentclass[11pt]{scrartcl}", file=f)
         print(r"\usepackage[sexy,diagrams]{evan}", file=f)
+        print(r"\author{" + VON_DEFAULT_AUTHOR + "}", file=f)
 
         print(r"\title{VON Preview}", file=f)
         print(r"\begin{document}", file=f)
