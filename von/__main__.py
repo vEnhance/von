86757168a2e6ba4a6382183baf55b27793786497
Evan Chen <evan@evanchen.cc>
2025-02-24 21:12:14 -0500

    feat!: make into a hatch package
    i have no idea what i'm doing help

--- von/__main__.py
+++ von/__main__.py
@@ -2,9 +2,14 @@ import sys
 
 from . import term
 
-if __name__ == "__main__":
+
+def main():
     cmd = term.VonTerminal()
     if len(sys.argv) == 1:
         cmd.run()
     else:
         cmd.direct(sys.argv[1:])  # to preserve command line quotes etc
+
+
+if __name__ == "__main__":
+    main()
