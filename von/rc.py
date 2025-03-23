33510e577903b506aa9447df75b37e7ced99bac3
Abel George Mathew <155311121+zvfrozel@users.noreply.github.com>
2025-03-23 17:55:26 +0530

    fix: Expanduser for VON_TMP_PATH as well (#35)
    * fix: VON tmp path expanduser
    
    * fix: fix it again

--- von/rc.py
+++ von/rc.py
@@ -126,7 +126,8 @@ else:
 
 VON_CUSTOM_LOOKUP = config.get("abbreviations", {})
 
-VON_TMP_PATH = config.get("von_tmp_path", tempfile.gettempdir())
+VON_TMP_PATH = str(Path(config.get("von_tmp_path", tempfile.gettempdir())).expanduser())
+
 # These used to be editable but I don't think it's worth it
 VON_INDEX_NAME = "index"
 VON_INDEX_PATH = os.path.join(VON_BASE_PATH, VON_INDEX_NAME)
